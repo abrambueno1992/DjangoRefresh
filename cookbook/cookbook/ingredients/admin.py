bd132fe4864dd5c4792eddc554c5ff3392cf0de4
Abraham Bueno <abueno@localhost.localdomain>
2018-09-19 21:49:14 -0500

    Completed part one of tutorial and uploaded requirements.txt

---
+++ cookbook/cookbook/ingredients/admin.py
@@ -0,0 +1,6 @@
+# from django.contrib import admin
+# from cookbook.ingredients.models import Category, Ingredients
+
+# admin.site.register(Category)
+# admin.site.register(Ingredient)
+# Register your models here.
